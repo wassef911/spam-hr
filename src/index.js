63c3945fd299d067fc7bd78f0bd82df30d2e939e
Wassef911 <1.wassef911@gmail.com>
2022-01-16 21:31:27 +0100

    init and finish

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
-const fs = require("fs");
-const fetchVideoById = require("./fetchVideoById");
-const { AsyncLoop } = require("./utils");
+const data = require("./in/data.json");
+const { composeAsync ,send} = require("./utils");
 
 (async () => {
   try {
-    const comData = await AsyncLoop(
-      require("./in/videosList.json"),
-      fetchVideoById
+    await composeAsync(
+      data.companies,
+      send
     );
-
-    comData.map((singleVideo, idx) => {
-      fs.writeFileSync(
-        "./src/out/video" + idx + ".json",
-        JSON.stringify(singleVideo, null, 2)
-      );
-    });
   } catch (err) {
     console.log(err);
   }
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,24 @@
-const AsyncLoop = async (arr, fn) => {
+const sgMail = require("@sendgrid/mail");
+const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+
+const send = ({ to, subject, text }) => {
+    try {
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+        const msg = {
+            to,
+            from: process.env.MY_EMAIL,
+            subject,
+            text
+        };
+        await sgMail.send(msg);
+        // C-- walle ? 5it...
+        console.log("email was sent : std::cout <<", to);
+    } catch (error) {
+        console.log(error.response.body);
+    }
+};
+
+const composeAsync = async (arr, fn) => {
   const ids = [];
   let id;
   for (let index = 0; index < arr.length; index++) {
@@ -8,4 +28,4 @@ const AsyncLoop = async (arr, fn) => {
   return ids;
 };
 
-module.exports = { AsyncLoop };
+module.exports = { composeAsync,send };
